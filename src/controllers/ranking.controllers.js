f751a361bc864d2720a3a504a7a39585532f3cff
mickalala <mickalarissa01@gmail.com>
2023-05-23 09:24:39 -0300

    configs/8: adding ranking

--- src/controllers/ranking.controllers.js
+++ src/controllers/ranking.controllers.js
@@ -1,14 +1,16 @@
+import { db } from "../database/database.connection.js"
 
 export async function ranking(req, res) {
     try {
-       const rank= await db.query(`SELECT  users.id, users.name, COALESCE(SUM(urls.visits),0) AS visitCount,
-       COALESCE(COUNT(urls."userId"),0) AS linksCount
+       const rank= await db.query(`SELECT  users.id, users.name,
+        COALESCE(COUNT(urls."userId"),0) AS "linksCount",
+        COALESCE(SUM(urls."visitCount"),0) AS "visitCount"
         FROM users LEFT JOIN urls 
         ON users.id = urls."userId"
         GROUP BY users.id
-        ORDER BY visitCount DESC
+        ORDER BY "visitCount" DESC
         LIMIT 10;`)
-        res.status(200).send(rank)
+        res.status(200).send(rank.rows)
     } catch (err) {
         res.status(500).send(err.message)
     }
--- src/routers/ranking.routes.js
+++ src/routers/ranking.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { ranking } from "../controllers/ranking.controllers.js";
 
 const rankingRouter= Router()
 
-rankingRouter.get("/ranking",)
+rankingRouter.get("/ranking",ranking)
 
 export default rankingRouter;
